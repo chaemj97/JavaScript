6d2b5510a8f08473ffdf9b5e2d07b357cd923af4
Chaemi720 <chaemj97@gmail.com>
2022-05-03 17:44:26 +0900

    0503workshop

---
+++ 0503workshop_practice/articles/views.py
@@ -0,0 +1,133 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+# @login_required
+@require_POST
+def delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user.is_authenticated:
+        if request.user == article.user: 
+            article.delete()
+            return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user == article.user:
+        if request.method == 'POST':
+            form = ArticleForm(request.POST, instance=article)
+            if form.is_valid():
+                form.save()
+                return redirect('articles:detail', article.pk)
+        else:
+            form = ArticleForm(instance=article)
+    else:
+        return redirect('articles:index')
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
+
+
+@require_POST
+def comments_create(request, pk):
+    if request.user.is_authenticated:
+        article = get_object_or_404(Article, pk=pk)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.article = article
+            comment.user = request.user
+            comment.save()
+        return redirect('articles:detail', article.pk)
+    return redirect('accounts:login')
+
+
+@require_POST
+def comments_delete(request, article_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.user:
+            comment.delete()
+    return redirect('articles:detail', article_pk)
+
+
+@require_POST
+def likes(request, article_pk):
+    # CODE HERE
+    # post 요청 받았으니까.....db에 적용
+    # 화면 구성에 필요한 데이터 만들어서 json으로 응답
+
+    # 화면 구성에 필요한 데이터 : 좋아요 했는가?, 좋아요 한 사람들 수
+    
+    # 로그인 된 사람만
+    if request.user.is_authenticated:
+        # 게시글 받아오기
+        article = get_object_or_404(Article, pk=article_pk)
+
+        # 사용자가 게시글의 좋아요 목록에 있으면 지우고 없으면 추가
+        if article.like_users.filter(pk=request.user.pk).exists():
+            article.like_users.remove(request.user)
+            liked = False
+        else:
+            article.like_users.add(request.user)
+            liked = True
+
+        # 화면 구성에 필요한 데이터
+        context = {
+            'liked' : liked,
+            'count' : article.like_users.count()
+        }
+        return JsonResponse(context)
+
+    return redirect('accounts:login')
