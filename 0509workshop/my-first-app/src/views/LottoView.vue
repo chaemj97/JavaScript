7da2474482d59b61d46525ebcaeef1c1521cd7f1
Chaemi720 <chaemj97@gmail.com>
2022-05-11 00:15:25 +0900

    0510수정

---
+++ 0509workshop/my-first-app/src/views/LottoView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>로또</h1>
+    <button @click="getLuckyNums">Get Lucky Numbers</button>
+    <p>{{ selectedLuckyNums }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name : 'LottoView',
+  data : function(){
+    return {
+      selectedLuckyNums : [],
+    }
+  },
+  methods:{
+    getLuckyNums:function(){
+      const numbers = _.range(1,46)
+      this.selectedLuckyNums = _.sampleSize(
+        numbers,
+        this.$route.params.lottoNum,
+      )
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 0509workshop/my-first-app/src/views/LunchView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>점심메뉴</h1>
+    <button @click="pickLunch">Pick Lunch</button>
+    <p>{{ selectedLunch }}</p>
+    <br>
+
+    <h2>로또를 뽑아보자</h2>
+    <button @click='goLotto'>Pick Lotto</button>
+
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name : 'LunchView',
+  data : function(){
+    return {
+      selectedLunch : '',
+    }
+  },
+  methods:{
+    pickLunch:function(){
+      this.selectedLunch = _.sample(['보쌈','피자','치킨','닭발'])
+    },
+    goLotto:function(){
+      this.$router.push({name:'lotto',params:{lottoNum:6}})
+    }
+  },
+}
+</script>
